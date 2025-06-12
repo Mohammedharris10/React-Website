f1bf19666a2a343f714c7de25cb738cd49c1c038
Mohammedharris10 <s.mohammedharris10@gmail.com>
2025-06-12 12:28:58 +0530

    Add Navigation Section

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { Navbar } from './component/navbar/Navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Navbar/>,
 )
